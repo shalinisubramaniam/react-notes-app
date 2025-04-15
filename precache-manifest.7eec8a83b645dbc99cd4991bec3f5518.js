0c798b17584c424bad1a5b1f8fb3d6c3ee8f9583
shalinisubramaniam <shalinisubramaniam234@gmail.com>
2025-04-16 00:55:13 +0530

    Updates

---
+++ precache-manifest.7eec8a83b645dbc99cd4991bec3f5518.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "778b69128a5a3c4b06d6ac3b004a7b30",
+    "url": "/react-notes-app/index.html"
+  },
+  {
+    "revision": "87228c8c04770daa7ae1",
+    "url": "/react-notes-app/static/css/main.9727fa2a.chunk.css"
+  },
+  {
+    "revision": "5a172c427a50c616bf42",
+    "url": "/react-notes-app/static/js/2.c8be2fa0.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/react-notes-app/static/js/2.c8be2fa0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87228c8c04770daa7ae1",
+    "url": "/react-notes-app/static/js/main.a6b7842c.chunk.js"
+  },
+  {
+    "revision": "c3c86199df052fb44ae5",
+    "url": "/react-notes-app/static/js/runtime-main.0c76a5c2.js"
+  }
+]);
